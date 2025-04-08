9ef2eecffad3e0f6e5025e19e1f99047b2a16333
Alireza Jafartash <alirezaopmc@gmail.com>
2025-04-08 10:57:28 +0330

    feat(neo-tree): toggle keybind

--- nvim/lua/plugins/editor/neo-tree.lua
+++ nvim/lua/plugins/editor/neo-tree.lua
@@ -1,3 +1,26 @@
+toggleNeoTree = function()
+    local neo_win = nil
+    local neo_open = false
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+        local buf = vim.api.nvim_win_get_buf(win)
+        local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+        if ft == "neo-tree" then
+            neo_open = true
+            neo_win = win
+            break
+        end
+    end
+    if not neo_open then
+        vim.cmd("Neotree show")
+    else
+        if vim.api.nvim_get_current_win() == neo_win then
+            vim.cmd("Neotree close")
+        else
+            vim.api.nvim_set_current_win(neo_win)
+        end
+    end
+end
+
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	version = "*",
@@ -9,7 +32,7 @@ return {
 	},
 	lazy = false, -- neo-tree will lazily load itself
 	keys = {
-		{ "\\", ":Neotree toggle<CR>", desc = "NeoTree reveal", silent = true },
+		{ "\\", "<cmd>lua toggleNeoTree()<CR>", desc = "NeoTree reveal", silent = true },
 	},
 	---@module 'neo-tree'
 	---@type neotree.Config?
