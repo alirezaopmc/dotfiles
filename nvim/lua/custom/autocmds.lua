21aabeeb1d57e5f05b7c65496adf34190293797d
Alireza Jafartash <alirezaopmc@gmail.com>
2025-04-16 13:13:09 +0330

    feat(keymaps): autosuggestion on insert mode

--- nvim/lua/custom/autocmds.lua
+++ nvim/lua/custom/autocmds.lua
@@ -11,3 +11,14 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.hl.on_yank()
 	end,
 })
+
+-- Tab size and shift settings for typescript files
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "typescript", "typescriptreact" },
+	callback = function()
+		vim.bo.shiftwidth = 2
+		vim.bo.tabstop = 2
+		vim.bo.softtabstop = 2
+		vim.bo.expandtab = true
+	end,
+})
--- nvim/lua/custom/keymaps.lua
+++ nvim/lua/custom/keymaps.lua
@@ -36,3 +36,6 @@ vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper win
 -- vim.keymap.set('n', '<C-S-l>', '<C-w>L', { desc = 'Move window to the right' })
 -- vim.keymap.set('n', '<C-S-j>', '<C-w>J', { desc = 'Move window to the lower' })
 -- vim.keymap.set('n', '<C-S-k>', '<C-w>K', { desc = 'Move window to the upper' })
+
+-- LSP Sign Help
+vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, { desc = "Signature Help" })
