b76c87e0d5610254bf0437952584e5ab1b4b8ae7
Alireza Jafartash <alirezaopmc@gmail.com>
2025-05-18 01:09:15 +0330

    fix: lsp kotlin fix

--- nvim/lua/plugins/lsp/nvim-lspconfig.lua
+++ nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -195,11 +195,49 @@ return {
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-		-- Import required plugins
-		local mason = require("mason")
-		local mason_lspconfig = require("mason-lspconfig")
-		local lspconfig = require("lspconfig")
-		local mason_tool_installer = require("mason-tool-installer")
+		-- Enable the following language servers
+		--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+		--
+		--  Add any additional override configuration in the following tables. Available keys are:
+		--  - cmd (table): Override the default command used to start the server
+		--  - filetypes (table): Override the default list of associated filetypes for the server
+		--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
+		--  - settings (table): Override the default settings passed when initializing the server.
+		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+		local servers = {
+			-- clangd = {},
+			-- gopls = {},
+			-- pyright = {},
+			-- rust_analyzer = {},
+			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+			--
+			-- Some languages (like typescript) have entire language plugins that can be useful:
+			--    https://github.com/pmizio/typescript-tools.nvim
+			--
+			-- But for many setups, the LSP (`ts_ls`) will work just fine
+			-- ts_ls = {},
+
+			kotlin_language_server = {
+				init_options = {
+					storagePath = vim.fn.stdpath("data") .. "/kotlin_language_server",
+				},
+			},
+
+			lua_ls = {
+				-- cmd = { ... },
+				-- filetypes = { ... },
+				-- capabilities = {},
+				settings = {
+					Lua = {
+						completion = {
+							callSnippet = "Replace",
+						},
+						-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+						-- diagnostics = { disable = { 'missing-fields' } },
+					},
+				},
+			},
+		}
 
 		-- Ensure the servers and tools above are installed
 		--
@@ -217,19 +255,26 @@ return {
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
 			"stylua", -- Used to format Lua code
+			"kotlin-language-server", -- Add kotlin-language-server to be installed by Mason
+			"ktlint", -- Optional: add ktlint for Kotlin formatting
 		})
-		mason_tool_installer.setup({ ensure_installed = ensure_installed })
+		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
-		mason_lspconfig.setup({
+		require("mason-lspconfig").setup({
 			ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
 			automatic_installation = false,
 			handlers = {
 				function(server_name)
-					local server = servers[server_name] or {}
+					-- local server = servers[server_name] or {}
+					local server = vim.deepcopy(servers[server_name] or {})
 					-- This handles overriding only values explicitly passed
 					-- by the server configuration above. Useful when disabling
 					-- certain features of an LSP (for example, turning off formatting for ts_ls)
 					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+					vim.schedule(function()
+						vim.notify("Setting up LSP for: " .. server_name)
+						vim.notify(vim.inspect(server))
+					end)
 					require("lspconfig")[server_name].setup(server)
 				end,
 			},
