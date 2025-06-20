37be3d4ce835af5c9cc90f5417854d0d47640050
Alireza Jafartash <alirezaopmc@gmail.com>
2025-06-20 14:34:19 +0330

    feat: kotlin fmt + update packages

--- nvim/lua/plugins/formatting/conform.lua
+++ nvim/lua/plugins/formatting/conform.lua
@@ -31,6 +31,7 @@ return { -- Autoformat
 		formatters_by_ft = {
 			lua = { "stylua" },
 			typescript = { "eslint_d", "prettierd" },
+			kotlin = { "ktfmt" },
 			-- Conform can also run multiple formatters sequentially
 			-- python = { "isort", "black" },
 			--
